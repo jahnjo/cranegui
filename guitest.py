f78948e85486879db676686d9cd16cdeccb65ab1
jahnjo <johndotfoster@gmail.com>
2019-02-21 10:35:04 -0500

    Added documentation, boom angle functionality, more color implementation for corner inclines and polygon itself

--- guitest.py
+++ guitest.py
@@ -1,3 +1,14 @@
+'''
+Cranemaster Realtime LMI GUI
+Inputs:
+    Incline X (roll) (units:deg) (range -20:20)
+    Incline Y (pitch) (units:deg) (range -20:20)
+    Outrigger Right (units:inches) (range 0:34)
+    Outrigger Left (units:inches) (range 0:34)
+    Boom Angle (units:deg) (range 0:75)
+'''
+
+
 from Tkinter import *
 import math
 import datetime
@@ -7,34 +18,33 @@ HEIGHT = 800
 CANVAS_MID_X = WIDTH/2
 CANVAS_MID_Y = HEIGHT/2
 SIDE = 400
+
 outriggerExtRight = 0
 outriggerExtLeft = 0
-inclX = -20
-inclY = -20
+inclX = 0
+inclY = 0
 deg = 0
+boomAngle = 0
+
 backIncX = True
 backIncY = True
+backBoom = True
 polyColor = "green"
 
 root = Tk()
 canvas = Canvas(root, bg="white", height=HEIGHT, width=WIDTH)
 canvas.pack(fill=BOTH, expand=1)
 
-staticBoomArm = [
-    [CANVAS_MID_X - 7, CANVAS_MID_Y - 350],
-    [CANVAS_MID_X + 7, CANVAS_MID_Y - 350],
-    [CANVAS_MID_X + 7, CANVAS_MID_Y - 20],
-    [CANVAS_MID_X - 7, CANVAS_MID_Y - 20],
-]
 
 
-
-vertice = [
-    [CANVAS_MID_X - SIDE/5, CANVAS_MID_Y - SIDE/6],
-    [CANVAS_MID_X + SIDE/6, CANVAS_MID_Y - SIDE/3],
-    [CANVAS_MID_X + SIDE/5, CANVAS_MID_Y + SIDE/2],
-    [CANVAS_MID_X - SIDE/3, CANVAS_MID_Y + SIDE/6],
-]
+def adjustBoom(midx, midy, boomAngle):
+    boomPoints = [
+        [CANVAS_MID_X - 7, CANVAS_MID_Y - 350 + (boomAngle*2.5)],
+        [CANVAS_MID_X + 7, CANVAS_MID_Y - 350 + (boomAngle*2.5)],
+        [CANVAS_MID_X + 7, CANVAS_MID_Y - 20],
+        [CANVAS_MID_X - 7, CANVAS_MID_Y - 20],
+    ]
+    return boomPoints
 
 def polygonColors(inclX, inclY):
     global polyColor
@@ -46,16 +56,43 @@ def polygonColors(inclX, inclY):
         polyColor = "green"
     return polyColor
 
+def colors(number):
+    color = "green"
+    if number > 14 or number < -14:
+        color = "red"
+    elif number > 7 or number < -7:
+        color = "yellow"
+    elif number < 7 or number > -7:
+        color = "green"
+    return color
 
+#Very important, takes in sensor values and modifies the shape of the polygon
+#based on outrigger pos and inclinometer x/y.
 def sensorInput(SIDE, midx, midy, orExtR, orExtL, inclX, inclY):
     vertices = [
         [CANVAS_MID_X - SIDE/2 - (orExtL*3) - (inclY*3), CANVAS_MID_Y - SIDE/2 + (inclX*3)],
         [CANVAS_MID_X + SIDE/2 + (orExtR*3) + (inclY*3), CANVAS_MID_Y - SIDE/2 - (inclX*3)],
-        [CANVAS_MID_X + SIDE/2 - (orExtR*3) - (inclY*3), CANVAS_MID_Y + SIDE/2 + (inclX*3)],
-        [CANVAS_MID_X - SIDE/2 + (orExtL*3) + (inclY*3), CANVAS_MID_Y + SIDE/2 - (inclX*3)],
+        [CANVAS_MID_X + SIDE/2 + (orExtR*3) - (inclY*3), CANVAS_MID_Y + SIDE/2 + (inclX*3)],
+        [CANVAS_MID_X - SIDE/2 - (orExtL*3) + (inclY*3), CANVAS_MID_Y + SIDE/2 - (inclX*3)],
     ]
     return vertices
-    
+
+def incrementBoomAngle():
+    global boomAngle
+    global backBoom
+    if backBoom:
+        if boomAngle > 75:
+            boomAngle = 75
+            backBoom = False
+        else:
+            boomAngle += 1
+    elif not backBoom:
+        if boomAngle < 0:
+            boomAngle = 0
+            backBoom = True
+        else:
+            boomAngle -= 1
+
 def incrementDeg():
     global deg
     if deg > 359:
@@ -148,11 +185,22 @@ def rotate(points, angle, center):
 def draw_square(points, polyColor):
     canvas.create_polygon(points, fill=polyColor, outline='black')
 
-def rotateAndDraw(rotDeg, basePoints, inclX, inclY):
+def rotateAndDraw(rotDeg, basePoints, inclX, inclY, midx, midy, boomAngle):
     #rewriting canvas
     canvas.delete("all")
 
-    polyColor = polygonColors(inclX, inclY)
+    #coloring the polygon based on the incline
+    #cornerColor = polygonColors(inclX, inclY)
+
+    #coloring the corner incline values based on its value
+    #colorX = colors(inclX)
+    #colorY = colors(inclY)
+
+    #default color
+    polyColor = "white"
+    colorX = "black"
+    colorY = "black"
+    
 
     #establishing center
     center = (CANVAS_MID_X, CANVAS_MID_Y)
@@ -172,10 +220,10 @@ def rotateAndDraw(rotDeg, basePoints, inclX, inclY):
     canvas.create_text((195,170),fill="black",font="Courier 20",text=rotDeg)
 
     #draw incline degrees
-    #canvas.create_text((100,50),fill="black",font="Courier 20",text="Incline X: ")
-    #canvas.create_text((195,50),fill="black",font="Courier 20",text=inclX)
-    #canvas.create_text((100,80),fill="black",font="Courier 20",text="Incline Y: ")
-    #canvas.create_text((195,80),fill="black",font="Courier 20",text=inclY)
+    canvas.create_text((100,50),fill="black",font="Courier 20",text="Incline X: ")
+    canvas.create_text((195,50),fill="black",font="Courier 20",text=inclX)
+    canvas.create_text((100,80),fill="black",font="Courier 20",text="Incline Y: ")
+    canvas.create_text((195,80),fill="black",font="Courier 20",text=inclY)
 
     #draw outrigger positions
     canvas.create_text((85,110),fill="black",font="Courier 20",text="OR Right:")
@@ -184,24 +232,34 @@ def rotateAndDraw(rotDeg, basePoints, inclX, inclY):
     canvas.create_text((195,140),fill="black",font="Courier 20",text=outriggerExtLeft)
 
 
-    #draw corner numbers
-    canvas.create_text(cornerPts[0],fill="red",font="Courier 30",text=inclY)
-    canvas.create_text(cornerPts[1],fill="red",font="Courier 30",text=inclY)
-    canvas.create_text(cornerPts[2],fill="blue",font="Courier 30",text=inclX)
-    canvas.create_text(cornerPts[3],fill="blue",font="Courier 30",text=inclX)
+    #draw corner numbers either , 1-4, or incline values (still under dev)
+    '''canvas.create_text(cornerPts[0],fill=colorY,font="Courier 30",text=inclY)
+    canvas.create_text(cornerPts[1],fill=colorX,font="Courier 30",text=inclX)
+    canvas.create_text(cornerPts[2],fill=colorX,font="Courier 30",text=inclX)
+    canvas.create_text(cornerPts[3],fill=colorY,font="Courier 30",text=inclY)'''
+
+    canvas.create_text(cornerPts[0],fill=colorY,font="Courier 30",text="1")
+    canvas.create_text(cornerPts[1],fill=colorX,font="Courier 30",text="2")
+    canvas.create_text(cornerPts[2],fill=colorX,font="Courier 30",text="3")
+    canvas.create_text(cornerPts[3],fill=colorY,font="Courier 30",text="4")
 
     #draw boom arm
+    staticBoomArm = adjustBoom(midx, midy, boomAngle)
     canvas.create_polygon(staticBoomArm, fill='white', outline='black')
 
 def clock():
+    #all increment functions simulate sensor input
     #incrementDeg()
     #incrementOr()
-    incrementInclineX()
-    incrementInclineY()
-    drawingPts = sensorInput(SIDE, CANVAS_MID_X, CANVAS_MID_Y, outriggerExtRight, outriggerExtLeft, inclX, inclY)   
-    rotateAndDraw(deg, drawingPts, inclX, inclY)
+    #incrementInclineX()
+    #incrementInclineY()
+    #incrementBoomAngle()
 
-    root.after(50, clock) # run itself again after 1000 ms
+    drawingPts = sensorInput(SIDE, CANVAS_MID_X, CANVAS_MID_Y, outriggerExtRight, outriggerExtLeft, inclX, inclY)   
+    rotateAndDraw(deg, drawingPts, inclX, inclY, CANVAS_MID_X, CANVAS_MID_Y, boomAngle)
+
+    #50, running at 20 Hz
+    root.after(50, clock) # run itself again after 50 ms
 
 
 
