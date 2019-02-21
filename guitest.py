9ab06e35622fb2c686d726cae6b3fbfef7490f7b
jahnjo <johndotfoster@gmail.com>
2019-02-20 21:09:52 -0500

    outrigger and incline functionality

--- guitest.py
+++ guitest.py
@@ -2,13 +2,18 @@ from Tkinter import *
 import math
 import datetime
 
-WIDTH = 800
+WIDTH = 1200
 HEIGHT = 800
 CANVAS_MID_X = WIDTH/2
 CANVAS_MID_Y = HEIGHT/2
-SIDE = WIDTH/2
-bigSIDE = WIDTH/1.9
+SIDE = 400
+outriggerExtRight = 0
+outriggerExtLeft = 0
+inclX = -20
+inclY = -20
 deg = 0
+backIncX = True
+backIncY = True
 
 root = Tk()
 canvas = Canvas(root, bg="white", height=HEIGHT, width=WIDTH)
@@ -21,12 +26,7 @@ staticBoomArm = [
     [CANVAS_MID_X - 7, CANVAS_MID_Y - 20],
 ]
 
-vertices = [
-    [CANVAS_MID_X - SIDE/2, CANVAS_MID_Y - SIDE/2],
-    [CANVAS_MID_X + SIDE/2, CANVAS_MID_Y - SIDE/2],
-    [CANVAS_MID_X + SIDE/2, CANVAS_MID_Y + SIDE/2],
-    [CANVAS_MID_X - SIDE/2, CANVAS_MID_Y + SIDE/2],
-]
+
 
 vertice = [
     [CANVAS_MID_X - SIDE/5, CANVAS_MID_Y - SIDE/6],
@@ -35,6 +35,66 @@ vertice = [
     [CANVAS_MID_X - SIDE/3, CANVAS_MID_Y + SIDE/6],
 ]
 
+def sensorInput(SIDE, midx, midy, orExtR, orExtL, inclX, inclY):
+    vertices = [
+        [CANVAS_MID_X - SIDE/2 - (orExtL*3) - (inclY*3), CANVAS_MID_Y - SIDE/2 + (inclX*3)],
+        [CANVAS_MID_X + SIDE/2 + (orExtR*3) + (inclY*3), CANVAS_MID_Y - SIDE/2 - (inclX*3)],
+        [CANVAS_MID_X + SIDE/2 - (orExtR*3) - (inclY*3), CANVAS_MID_Y + SIDE/2 + (inclX*3)],
+        [CANVAS_MID_X - SIDE/2 + (orExtL*3) + (inclY*3), CANVAS_MID_Y + SIDE/2 - (inclX*3)],
+    ]
+    return vertices
+    
+def incrementDeg():
+    global deg
+    if deg > 359:
+        deg = 0
+    else:
+        deg += 1
+
+def incrementOr():
+    global outriggerExtRight
+    global outriggerExtLeft
+    if outriggerExtRight > 34:
+        outriggerExtRight = 0
+    else:
+        outriggerExtRight += 1
+    
+    if outriggerExtLeft > 34:
+        outriggerExtLeft = 0
+    else:
+        outriggerExtLeft += 1
+
+def incrementInclineX():
+    global inclX
+    global backIncX
+    if backIncX:
+        if inclX > 20:
+            inclX = 20
+            backIncX = False
+        else:
+            inclX += 1
+    elif not backIncX:
+        if inclX < -20:
+            inclX = -20
+            backIncX = True
+        else:
+            inclX -= 1
+
+def incrementInclineY():
+    global inclY
+    global backIncY
+    if backIncY:
+        if inclY > 20:
+            inclY = 20
+            backIncY = False
+        else:
+            inclY += 1
+    elif not backIncY:
+        if inclY < -20:
+            inclY = -20
+            backIncY = True
+        else:
+            inclY -= 1
 
 def createCorners(points):
     cornerPoints = []
@@ -76,14 +136,7 @@ def rotate(points, angle, center):
 def draw_square(points, color="white"):
     canvas.create_polygon(points, fill=color, outline='black')
 
-def incrementDeg():
-    global deg
-    if deg > 359:
-        deg = 0
-    else:
-        deg += 1
-
-def rotateAndDraw(rotDeg, basePoints):
+def rotateAndDraw(rotDeg, basePoints, inclX, inclY):
     #rewriting canvas
     canvas.delete("all")
 
@@ -101,7 +154,21 @@ def rotateAndDraw(rotDeg, basePoints):
     draw_square(newSquare)
 
     #draw degrees in center
-    canvas.create_text(center,fill="black",font="Courier 20",text=rotDeg)
+    canvas.create_text((77,170),fill="black",font="Courier 20",text="Degrees:")
+    canvas.create_text((195,170),fill="black",font="Courier 20",text=rotDeg)
+
+    #draw incline degrees
+    canvas.create_text((100,50),fill="black",font="Courier 20",text="Incline X: ")
+    canvas.create_text((195,50),fill="black",font="Courier 20",text=inclX)
+    canvas.create_text((100,80),fill="black",font="Courier 20",text="Incline Y: ")
+    canvas.create_text((195,80),fill="black",font="Courier 20",text=inclY)
+
+    #draw outrigger positions
+    canvas.create_text((85,110),fill="black",font="Courier 20",text="OR Right:")
+    canvas.create_text((195,110),fill="black",font="Courier 20",text=outriggerExtRight)
+    canvas.create_text((85,140),fill="black",font="Courier 20",text="OR Left: ")
+    canvas.create_text((195,140),fill="black",font="Courier 20",text=outriggerExtLeft)
+
 
     #draw corner numbers
     canvas.create_text(cornerPts[0],fill="red",font="Courier 30",text='1')
@@ -114,9 +181,15 @@ def rotateAndDraw(rotDeg, basePoints):
 
 def clock():
     incrementDeg()
-    rotateAndDraw(deg, vertices)
+    #incrementOr()
+    incrementInclineX()
+    incrementInclineY()
+    drawingPts = sensorInput(SIDE, CANVAS_MID_X, CANVAS_MID_Y, outriggerExtRight, outriggerExtLeft, inclX, inclY)   
+    rotateAndDraw(deg, drawingPts, inclX, inclY)
 
     root.after(50, clock) # run itself again after 1000 ms
+
+
 
 clock()
 root.mainloop()
